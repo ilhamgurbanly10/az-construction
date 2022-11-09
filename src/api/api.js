7c453cd0d34f1b08e898ac4dd0fa9e539788e16d
murad-narimanli <murad.narimanli@mail.ru>
2022-11-09 19:20:00 +0400

    changes

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-  baseURL:  `http://localhost:3000`,
+  baseURL:  `https://imat-admin.herokuapp.com/api`,
   headers: { "Content-Type": "application/json" },
 });
 
